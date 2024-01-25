cbace8af24c0b5eed0fda8580d483e7d2a7ee4dd
Jon Attfield <jon.attfield@gmail.com>
2024-01-25 19:47:34 +0000

    Initial Commit

---
+++ MetalBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetalBase
+//
+//  Created by Jonathan Attfield on 25/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalBase/MetalBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalBaseApp.swift
+//  MetalBase
+//
+//  Created by Jonathan Attfield on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MetalBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
